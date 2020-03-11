00c46eb03bae567501950aae7e1c90a34afa5e21
DevBara <61251493+DevBara@users.noreply.github.com>
2020-03-11 14:00:38 -0500

    pseudoCode
    This file shows what I belive is needed to make my game functional. This does not include styling, only javascript

---
+++ pseudocode.js
@@ -0,0 +1,30 @@
+//Ship Types
+//Cruiser = 4 health 
+//Submarine = 3 health 
+//Destroyer = 1 health 
+//Total of 8 health per player- When this number is reached, game over
+
+//Gameboard 8x8
+//# of rows= 8
+//# of columns = 8
+//little squares within gameboard = 40
+
+//Variables
+//healthCount
+//gameboard creation
+
+
+//a 2 loops for columns & rows
+// for (loop for columns)
+//     for (loop for rows within column loop)
+
+//Eventlistener
+//once gameboard is clicked, fire a shot
+//If a player fires a shot on a square that doesnt have a ship, it should change to red.
+        //console.log('You missed')
+//If a player fires a shot on a square that does have a ship,
+        //console.log('You have hit a ship')
+
+//function Gameover
+   // if( healthCount == 8)
+        //console.log("you have destroyed all the ships! You have won")
