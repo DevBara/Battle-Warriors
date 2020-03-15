e89dbd8f2112cdaf35ebb84027fbb7ef9f1f3728
DevBara <61251493+DevBara@users.noreply.github.com>
2020-03-15 16:24:59 -0500

    update code

--- main.js
+++ main.js
@@ -1,46 +1,10 @@
-let rows = 10;
-let columns = 10;
-let smallSquares= 40;
 
-let gameBoard =document.getElementById('gameboard');
+let restartButton;
 
 
-
-//Gameboard need to be in an array to determine whats hit
-// 1 = part of ship, 0 = no ship
-
- console.log("gameBoard");
-
-
- //draw 10x10 grid
- let newArray =[]
-
- for ( let i = 0; i > 10; i++){
-     newArray[i] = [];
-     for(let k = 0; k > 10; k++){
-         newArray[i][k] = k; 
-     }
- }
-// defined variable fills the newArray with predetermined info
- let gameboard = [
-    [0,0,1,1,1,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,1],
-    [0,0,0,0,0,0,0,0,0,1],
-    [0,0,0,0,0,0,0,0,0,1],
-    [0,0,0,0,0,0,0,0,0,1],
-    [0,0,1,0,0,0,0,0,0,0],
-    [0,0,1,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0]
-];
-
-console.log(gameboard);
-
-//Used jquery to build modal with the assistance of twitter coders
 // Originally I used getElementByClass, however doing so, elements were not calling correctly 
 let modal = document.querySelector('.modal');
-let openModal = document.querySelector('.howto')
+let openModal = document.querySelector('.howto');
 let closeModal = document.querySelector('.close');
 
 function toggleModal(){
@@ -53,16 +17,15 @@ function windowOnClick(event){
     }
 }
 
-//Call on functions via EventListeners
+//Call on functions via EventListeners for Modal
 openModal.addEventListener("click", toggleModal);
 closeModal.addEventListener("click", toggleModal);
 window.addEventListener("click", windowOnClick);
+//end modal functions & letiable declarations 
 
-
-//This will start the game
-// function start(){
-//     document.getElementById('start').onclick
-//     console.log("start");
-
-// }
+// This will start the game
+function start(){
+    document.getElementById('start-button-container').onclick
+    console.log("start");
+}
 
