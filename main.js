2a1a44cb77ebfefe4be7016ea2b489773edd7ca2
DevBara <61251493+DevBara@users.noreply.github.com>
2020-03-16 11:23:28 -0500

    Created function to show/hide boards

--- main.js
+++ main.js
@@ -16,13 +16,39 @@ function windowOnClick(event){
     }
 }
 
-//Call on functions via EventListeners for Modal
+//Call on functions via EventListeners for show instructions Modal
 openModal.addEventListener("click", toggleModal);
 closeModal.addEventListener("click", toggleModal);
 window.addEventListener("click", windowOnClick);
 //end modal function
 
 //On start button click, show gameboards
+document.getElementById("startButton").addEventListener("click",function()
+{
+    let showBoard= document.getElementById("player-1-board");
+    if(showBoard.style.display=="none"){
+        showBoard.style.display ="table";
+    } else{
+        showBoard.style.display="none";
+    }
+
+})
+
+document.getElementById("startButton").addEventListener("click",function()
+{
+    let showBoard= document.getElementById("player-2-board");
+    if(showBoard.style.display=="none"){
+        showBoard.style.display ="table";
+    } else{
+        showBoard.style.display="none";
+    }
+
+})
+    // let a variable equal to start id, grab it
+  
+    // Once that is grab if styling says Hide button will show board
+
+
 
 
 
