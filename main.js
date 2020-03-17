497e9de5afd26e338ab658f1862fc2721541ddb8
DevBara <61251493+DevBara@users.noreply.github.com>
2020-03-16 19:56:20 -0500

    Jquery Change Color

--- main.js
+++ main.js
@@ -1,6 +1,20 @@
 //Variables
 
 
+$(document).ready(function(){
+    $(".one").on({
+        
+        click:function(){
+            $(this).css("background-color","red");
+        }
+    });
+
+});
+
+//
+$("td").click(function(){
+    $(this).change();
+  });    
 // Originally I used getElementByClass, however doing so, elements were not calling correctly 
 let modal = document.querySelector('.modal');
 let openModal = document.querySelector('.howto');
@@ -16,18 +30,41 @@ function windowOnClick(event){
     }
 }
 
-//Call on functions via EventListeners for Modal
+//Call on functions via EventListeners for show instructions Modal
 openModal.addEventListener("click", toggleModal);
 closeModal.addEventListener("click", toggleModal);
 window.addEventListener("click", windowOnClick);
 //end modal function
 
-//On start button click, show gameboards
+//Show Player 1 Board
+document.getElementById("startButton").addEventListener("click",function()
+{
+    let showBoard= document.getElementById("boardOne");
+    if(showBoard.style.display=="none"){
+        showBoard.style.display ="table";
+    } else{
+        showBoard.style.display="none";
+        document.getElementById('startContainer').classList.add('show');
+    }
 
+})
+//Player 2 Board
+document.getElementById("startButton").addEventListener("click",function()
+{
+    let showBoard= document.getElementById("boardTwo");
+    if(showBoard.style.display=="none"){
+        showBoard.style.display ="table";
+    } else{
+        showBoard.style.display="none";
+    }
 
+})
 
-//Function that determines when the game is won
-
-
-//Function that gives player ability to restart
-
+//Hide buttons on start click
+function hideStart(){
+    
+    // document.getElementById('startContainer').classList.add('show');
+    document.getElementById('how').classList.add('hide');
+    document.getElementById('rematchButton').classList.add('hide');
+    break;
+}
