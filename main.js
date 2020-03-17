1acf9b27d57d1215e066282594e14c116f2d8478
DevBara <61251493+DevBara@users.noreply.github.com>
2020-03-16 20:35:45 -0500

    Jquery change color

--- main.js
+++ main.js
@@ -1,20 +1,16 @@
 //Variables
 
 
+
 $(document).ready(function(){
     $(".one").on({
-        
+
         click:function(){
             $(this).css("background-color","red");
         }
     });
 
 });
-
-//
-$("td").click(function(){
-    $(this).change();
-  });    
 // Originally I used getElementByClass, however doing so, elements were not calling correctly 
 let modal = document.querySelector('.modal');
 let openModal = document.querySelector('.howto');
@@ -36,32 +32,21 @@ closeModal.addEventListener("click", toggleModal);
 window.addEventListener("click", windowOnClick);
 //end modal function
 
-<<<<<<< HEAD
-//Show Player 1 Board
-document.getElementById("startButton").addEventListener("click",function()
-{
-    let showBoard= document.getElementById("boardOne");
-=======
 //On start button click, show gameboards
 document.getElementById("startButton").addEventListener("click",function()
 {
-    let showBoard= document.getElementById("player-1-board");
->>>>>>> 2a1a44cb77ebfefe4be7016ea2b489773edd7ca2
+    let showBoard= document.getElementById("boardOne");
     if(showBoard.style.display=="none"){
         showBoard.style.display ="table";
     } else{
         showBoard.style.display="none";
-<<<<<<< HEAD
-        document.getElementById('startContainer').classList.add('show');
-    }
-=======
     }
 
 })
 
 document.getElementById("startButton").addEventListener("click",function()
 {
-    let showBoard= document.getElementById("player-2-board");
+    let showBoard= document.getElementById("boardTwo");
     if(showBoard.style.display=="none"){
         showBoard.style.display ="table";
     } else{
@@ -74,30 +59,15 @@ document.getElementById("startButton").addEventListener("click",function()
     // Once that is grab if styling says Hide button will show board
 
 
+    function hideStart(){
 
+        // document.getElementById('startContainer').classList.add('show');
+        document.getElementById('how').classList.add('hide');
+        document.getElementById('rematchButton').classList.add('hide');
+    }
 
 
 //Function that determines when the game is won
->>>>>>> 2a1a44cb77ebfefe4be7016ea2b489773edd7ca2
 
-})
-//Player 2 Board
-document.getElementById("startButton").addEventListener("click",function()
-{
-    let showBoard= document.getElementById("boardTwo");
-    if(showBoard.style.display=="none"){
-        showBoard.style.display ="table";
-    } else{
-        showBoard.style.display="none";
-    }
 
-})
-
-//Hide buttons on start click
-function hideStart(){
-    
-    // document.getElementById('startContainer').classList.add('show');
-    document.getElementById('how').classList.add('hide');
-    document.getElementById('rematchButton').classList.add('hide');
-    break;
-}
+//Function that gives player ability to restart
