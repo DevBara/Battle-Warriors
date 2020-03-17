6922a5f4cb05c5b3e8cf0bdb71156a6e41e53f96
DevBara <61251493+DevBara@users.noreply.github.com>
2020-03-17 01:08:45 -0500

    functional

--- main.js
+++ main.js
@@ -1,16 +1,11 @@
 //Variables
 
 
+//Ship Types
+// Cruiser = 3
+// Submarine = 2
+// Destroyer = 1
 
-$(document).ready(function(){
-    $(".one").on({
-
-        click:function(){
-            $(this).css("background-color","red");
-        }
-    });
-
-});
 // Originally I used getElementByClass, however doing so, elements were not calling correctly 
 let modal = document.querySelector('.modal');
 let openModal = document.querySelector('.howto');
@@ -32,6 +27,9 @@ closeModal.addEventListener("click", toggleModal);
 window.addEventListener("click", windowOnClick);
 //end modal function
 
+
+
+
 //On start button click, show gameboards
 document.getElementById("startButton").addEventListener("click",function()
 {
@@ -43,31 +41,94 @@ document.getElementById("startButton").addEventListener("click",function()
     }
 
 })
-
-document.getElementById("startButton").addEventListener("click",function()
-{
-    let showBoard= document.getElementById("boardTwo");
-    if(showBoard.style.display=="none"){
-        showBoard.style.display ="table";
-    } else{
-        showBoard.style.display="none";
+function hitTarget(){
+    let target= document.getElementsByClassName('hit');
+    for(let item of target){
+        item.style.backgroundColor="red";
+        alert("Hit!");
     }
+}
 
-})
-    // let a variable equal to start id, grab it
-  
-    // Once that is grab if styling says Hide button will show board
-
-
-    function hideStart(){
-
-        // document.getElementById('startContainer').classList.add('show');
-        document.getElementById('how').classList.add('hide');
-        document.getElementById('rematchButton').classList.add('hide');
+function hitTarget1(){
+    let target= document.getElementsByClassName('hit1');
+    for(let item of target){
+        item.style.backgroundColor="red";
+        alert("Hit!");
     }
+}
 
+function hitTarget2(){
+    let target= document.getElementsByClassName('hit2');
+    for(let item of target){
+        item.style.backgroundColor="red";
+        alert("Hit!");
+    }
+}
+function hitTarget3(){
+    let target= document.getElementsByClassName('hit3');
+    for(let item of target){
+        item.style.backgroundColor="red";
+        alert("Hit!");
+    }
+}
+function hitTarget4(){
+    let target= document.getElementsByClassName('hit4');
+    for(let item of target){
+        item.style.backgroundColor="red";
+        alert("Hit!");
+    }
+}
+function hitTarget5(){
+    let target= document.getElementsByClassName('hit5');
+    for(let item of target){
+        item.style.backgroundColor="red";
+        alert("Player 2 Ship has been hit!");
+    }
+}
+function hitTarget6(){
+    let target= document.getElementsByClassName('hit6');
+    for(let item of target){
+        item.style.backgroundColor="red";
+        alert("Player 2 Ship has been hit!");
+    }
+}
+function hitTarget7(){
+    let target= document.getElementsByClassName('hit7');
+    for(let item of target){
+        item.style.backgroundColor="red";
+        alert("Player 2 Ship has been hit!");
+    }
+}
+function hitTarget8(){
+    let target= document.getElementsByClassName('hit8');
+    for(let item of target){
+        item.style.backgroundColor="red";
+        alert("Player 2 Ship has been hit!");
+    }
+}
+function hitTarget9(){
+    let target= document.getElementsByClassName('hit9');
+    for(let item of target){
+        item.style.backgroundColor="red";
+        alert("Player 2 Ship has been hit!");
+    }
+}
+function hitTarget10(){
+    let target= document.getElementsByClassName('hit10');
+    for(let item of target){
+        item.style.backgroundColor="red";
+        alert("Player 2 Ship has been hit!");
+    }
+}
 
-//Function that determines when the game is won
+// function winGame{
 
+// }
+        
 
-//Function that gives player ability to restart
+    // function hideStart(){
+
+    //     document.getElementById('startContainer').classList.add('show');
+    //     document.getElementById('how').classList.add('hide');
+    //     document.getElementById('rematchButton').classList.add('hide');
+    // }
